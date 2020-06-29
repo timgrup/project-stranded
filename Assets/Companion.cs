39d17d7fc34a9c6073d138cdcc2ce657225b7146
Tim Grupczynski <t.grupczynski@googlemail.com>
2020-06-30 00:26:32 +0200

    Improved Companion Movement

--- Assets/Companion.cs
+++ Assets/Companion.cs
@@ -11,7 +11,7 @@ public class Companion : MonoBehaviour
     {
         if (Vector3.Distance(transform.position, playerPivot.transform.position) > Mathf.Epsilon)
         {
-            var direction = Vector3.MoveTowards(transform.position, playerPivot.transform.position, speed * Time.deltaTime);
+            var direction = Vector3.MoveTowards(transform.position, playerPivot.transform.position, speed * Vector3.Distance(transform.position, playerPivot.transform.position) * Time.deltaTime);
             transform.position = direction;
 
             transform.LookAt(playerPivot.TransformPoint(Vector3.forward * 0.1f));
